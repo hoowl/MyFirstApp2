d4cf6631e6ba90e47140315bed23b43e9de25f2b
hoowl <hoowl@acd.tarc.edu.my>
2016-10-23 13:38:30 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,24 @@
+package my.edu.tarc.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        setContentView(textView);
+    }
+}
